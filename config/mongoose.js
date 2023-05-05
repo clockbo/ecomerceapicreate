262c407efa0a5064d5e69fd531776a06b8ab7d0a
baba <sagar.ktechgmail.com>
2023-05-05 17:59:21 +0530

    creating ecomerce api

---
+++ config/mongoose.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+// connecting mongoose to its default server and ecommerceDB
+mongoose.connect(`mongodb://0.0.0.0/backendprojectin_habbittrackejkahsjaskar_db`);
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+db.once('open', function(){
+    console.log('Connected to Database :: MongoDB');
+});
+
+module.exports = db;
